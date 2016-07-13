89c02a4675e128b86fbe147e77ee1da52c9f8513
fabio.ercoli <fercoli@redhat.com>
2016-07-13 20:07:18 +0200

    client ejb

--- eap6-ejb-remote-invoker/src/main/java/it/redhat/demo/service/EjbClient.java
+++ eap6-ejb-remote-invoker/src/main/java/it/redhat/demo/service/EjbClient.java
@@ -1,5 +1,14 @@
 package it.redhat.demo.service;
 
+import it.redhat.demo.NumberApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Hashtable;
+
 /**
  * @author Fabio Massimo Ercoli
  *         fabio.ercoli@redhat.com
@@ -7,8 +16,26 @@ package it.redhat.demo.service;
  */
 public class EjbClient {
 
-    public String go() {
-        return "CIAO GO ";
+    private Logger log = LoggerFactory.getLogger(EjbClient.class);
+
+    public Integer go() {
+
+        final Hashtable<String, String> jndiProperties = new Hashtable<String, String>();
+        jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+
+        try {
+
+            final Context context = new InitialContext(jndiProperties);
+            NumberApi numberApi = (NumberApi) context.lookup("java:global/eap6-ejb-remote/NumberApiBean!it.redhat.demo.NumberApi");
+            return numberApi.getNext();
+
+        } catch (NamingException e) {
+
+            log.error(e.getMessage(), e);
+
+        }
+
+        return null;
     }
 
 }
