9ff4037aa80607f1278fb0d7d0cc3bff9896614d
fax4ever <fabiomassimo.ercoli@gmail.com>
2016-11-06 10:51:49 +0100

    db security realm sample

---
+++ eap6-db-security-realm/src/test/resources/create.sql
@@ -0,0 +1,27 @@
+CREATE TABLE `fabio`.`users` (
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`username`),
+  UNIQUE INDEX `idusers_UNIQUE` (`username` ASC));
+
+CREATE TABLE `fabio`.`roles` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NULL,
+  `rolename` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  INDEX `user_idx` (`username` ASC),
+  CONSTRAINT `user`
+  FOREIGN KEY (`username`)
+  REFERENCES `fabio`.`users` (`username`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+INSERT INTO `fabio`.`users` (`username`, `password`) VALUES ('fabio', 'fabio');
+INSERT INTO `fabio`.`users` (`username`, `password`) VALUES ('chiara', 'chiara');
+INSERT INTO `fabio`.`roles` (`username`, `rolename`) VALUES ('fabio', 'admin');
+INSERT INTO `fabio`.`roles` (`username`, `rolename`) VALUES ('fabio', 'user');
+INSERT INTO `fabio`.`roles` (`username`, `rolename`) VALUES ('chiara', 'manager');
+
+SELECT password FROM fabio.users where username='chiara';
+SELECT rolename FROM fabio.roles where username='chiara';
