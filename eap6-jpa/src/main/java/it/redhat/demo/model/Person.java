9d8d4251658b473633822eae4e3e968f584f67ee
fax4ever <fabiomassimo.ercoli@gmail.com>
2016-11-07 17:06:33 +0100

    adding dependent entity

--- eap6-jpa/src/main/java/it/redhat/demo/model/Person.java
+++ eap6-jpa/src/main/java/it/redhat/demo/model/Person.java
@@ -2,11 +2,11 @@ package it.redhat.demo.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -17,6 +17,9 @@ public class Person implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Valid
+	private Address address; 
 	
 	@NotNull
 	@Size(min = 1, max = 25)
