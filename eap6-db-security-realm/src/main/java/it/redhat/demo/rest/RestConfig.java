132fb199fb24cd4f593b22481cd881e7fca1711d
fax4ever <fabiomassimo.ercoli@gmail.com>
2016-11-08 07:12:26 +0100

    secure ejb

---
+++ eap6-db-security-realm/src/main/java/it/redhat/demo/rest/RestConfig.java
@@ -0,0 +1,9 @@
+package it.redhat.demo.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class RestConfig extends Application {
+
+}
