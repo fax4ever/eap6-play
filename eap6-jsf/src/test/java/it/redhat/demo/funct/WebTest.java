72c48c9d6b14356dc9770f7310bbeb5dba94dbe7
fax4ever <fabiomassimo.ercoli@gmail.com>
2016-11-12 07:46:11 +0100

    jsf functional test

--- eap6-jsf/src/test/java/it/redhat/demo/funct/WebTest.java
+++ eap6-jsf/src/test/java/it/redhat/demo/funct/WebTest.java
@@ -1,10 +1,12 @@
 package it.redhat.demo.funct;
 
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.archive.importer.MavenImporter;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -12,13 +14,23 @@ import org.junit.runner.RunWith;
  */
 
 @RunWith(Arquillian.class)
-public class WebFunct {
+public class WebTest {
 
-    @Deployment
+    @Deployment(testable = false)
     public static WebArchive createDeployment() {
 
-        return ShrinkWrap.create(MavenImporter.class).loadPomFromFile("pom.xml").importBuildOutput()
+        WebArchive war = ShrinkWrap.create(MavenImporter.class).loadPomFromFile("pom.xml").importBuildOutput()
                 .as(WebArchive.class);
+
+        System.out.println(war);
+
+        return war;
+
+    }
+
+    @Test
+    @RunAsClient
+    public void test() {
 
     }
 
