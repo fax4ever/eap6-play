060c7ae8eb48482922f2a3bd4f218e00b883cc72
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-06-08 11:35:23 +0200

    role principal in user helper project

--- eap6-principal-roles/src/main/java/it/redhat/demo/roles/rest/RestService.java
+++ eap6-principal-roles/src/main/java/it/redhat/demo/roles/rest/RestService.java
@@ -5,12 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ejb.Stateless;
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 import java.security.Principal;
 
 /**
@@ -24,16 +24,20 @@ public class RestService {
 
     private Logger LOG = LoggerFactory.getLogger(RestService.class);
 
-    private final static String[] ALL_ROLES = {"admin", "analyst", "manager", "kie-server", "VAGE-GI", "VASE-GI", "VASS-GI", "VAUT-GI", "VCM1-GI", "VCM2-GI", "VVAC-GI", "VVCC-GI", "VVSC-GI"};
+    private final static String[] ALL_ROLES = { "admin", "AGENTE", "AMMINISTRATORE", "analyst", "DIREZIONALE", "kie-server", "manager", "MEDICO", "VAGE-GI", "VASE-GI", "VASS-GI", "VAUT-GI", "VCM1-GI", "VCM2-GI", "VENDITORE", "VVAC-GI", "VVCC-GI", "VVSC-GI" };
 
     @Context
-    private HttpServletRequest servletRequest;
+    private SecurityContext securityContext;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public PrincipalInfo getSecurityContext() {
 
-        Principal principal = servletRequest.getUserPrincipal();
+        if (securityContext == null) {
+            return new PrincipalInfo();
+        }
+
+        Principal principal = securityContext.getUserPrincipal();
 
         if (principal == null) {
             return new PrincipalInfo();
@@ -43,12 +47,10 @@ public class RestService {
 
         PrincipalInfo principalInfo = new PrincipalInfo(principal.getName());
 
-        LOG.info("{}", servletRequest.getClass());
-
         for (int i = 0; i < ALL_ROLES.length; i++) {
             String role = ALL_ROLES[i];
 
-            if (servletRequest.isUserInRole(role)) {
+            if (securityContext.isUserInRole(role)) {
                 principalInfo.addRole(role);
             }
 
